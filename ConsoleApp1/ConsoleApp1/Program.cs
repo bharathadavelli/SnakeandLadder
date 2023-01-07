8ef9319d8293635ba1cce237eb0cb9f22e79bcb7
“bharathadavelli” <bharathadavelli@gmail.com>
2023-01-07 21:43:21 +0530

    [Bharath]-Rolldie

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,11 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Snake & Ladder Simulator");
-
             int position = 0;
-
             Console.WriteLine("Welcome Player 1");
             Console.WriteLine("Player 1 is at position : " + position);
+
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine("Player rolls a die and get : {0}", die);
 
         }
     }
