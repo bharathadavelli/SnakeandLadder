1a91f04f9da68d726f57b7bc7a112006449c8585
“bharathadavelli” <bharathadavelli@gmail.com>
2023-01-07 22:02:54 +0530

    [Bharath]-Twoplayers

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace SnakeAndLadder
+﻿using System;
+namespace SnakeAndLadder
 {
     internal class Program
     {
@@ -10,25 +11,52 @@
             Console.WriteLine("Player 1 is at position : " + position);
 
             Random random = new Random();
-            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine("Player rolls a die and get : {0}", die);
 
-            while (position <= 100)
+            int check = random.Next(0, 3);
+
+            if (check == 0)
             {
-
-                int temp = position;
-
-                int die = random.Next(1, 7);
-                Console.WriteLine("Player rolls a die and get : {0}", die);
-@@ -40,6 + 40,10 @@ static void Main(string[] args)
-                {
-                    position = 0;
-                }
-                else if (position > 100)
-                {
-                    position = temp;
-                }
-                Console.WriteLine("Player current position is : " + position);
+                Console.WriteLine("No Play");
             }
+            else if (check == 1)
+            {
+                Console.WriteLine("Player Moves Ahead by {0} from ladder", die);
+                position += die;
+            }
+            else
+                while (position <= 100)
+                {
+                    Console.WriteLine("Player Moves Behind by {0} from Snake", die);
+                    position -= die;
+
+
+                    int die = random.Next(1, 7);
+                    Console.WriteLine("Player rolls a die and get : {0}", die);
+
+                    int check = random.Next(0, 3);
+
+                    if (check == 0)
+                    {
+                        Console.WriteLine("No Play");
+                    }
+                    else if (check == 1)
+                    {
+                        Console.WriteLine("Player Moves Ahead by {0} from ladder", die);
+                        position += die;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player Moves Behind by {0} from Snake", die);
+                        position -= die;
+                    }
+                    if (position < 0)
+                    {
+                        position = 0;
+                    }
+                    Console.WriteLine("Player current position is : " + position);
+                }
             Console.WriteLine("Current Position is : {0}", position);
         }
     }
