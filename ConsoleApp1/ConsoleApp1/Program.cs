7704db8aa20ec3b84ca080ee289ef24b0d606bff
“bharathadavelli” <bharathadavelli@gmail.com>
2023-01-07 21:46:28 +0530

    [Bharath]-Playercheck

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,11 +8,27 @@
             int position = 0;
             Console.WriteLine("Welcome Player 1");
             Console.WriteLine("Player 1 is at position : " + position);
-
             Random random = new Random();
             int die = random.Next(1, 7);
             Console.WriteLine("Player rolls a die and get : {0}", die);
 
+            int check = random.Next(0, 3);
+
+            if (check == 0)
+            {
+                Console.WriteLine("No Play");
+            }
+            else if (check == 1)
+            {
+                Console.WriteLine("Player Moves Ahead by {0} from ladder", die);
+                position += die;
+            }
+            else
+            {
+                Console.WriteLine("Player Moves Behind by {0} from Snake", die);
+                position -= die;
+            }
+            Console.WriteLine("Current Position is : {0}", position);
         }
     }
 }
